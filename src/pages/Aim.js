483df47f511a05baebeab610cacfa5771dd13ce2
mahir mehta <mahirmehta77@gmail.com>
2023-04-15 12:25:34 +0530

    Update Aim.js

--- src/pages/Aim.js
+++ src/pages/Aim.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Aim() {
   return (
-    <div>Aim</div>
+    <div className="container" align="center">
+      <br/>
+      <h4>Aim</h4>
+      <br/>
+      <p align="justify">
+      <b>Aim: </b>To Simulate virtually and Interpret Binary Phase Shift Keying waveforms.
+      </p>
+    </div>
   )
 }
 
