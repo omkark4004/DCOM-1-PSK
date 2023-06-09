007a8279f8047d881b26c816c8f88cf16d42b2b6
mahir mehta <mahirmehta77@gmail.com>
2023-06-09 12:00:12 +0530

    Update Aim.js

--- src/pages/Aim.js
+++ src/pages/Aim.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
 
 function Aim() {
   return (
-    <div className="container" align="center">
-      <br/>
-      <h4>Aim</h4>
-      <br/>
-      <p align="justify">
-      <b>Aim: </b>To Simulate virtually and Interpret Binary Phase Shift Keying waveforms.
-      </p>
-    </div>
+    <Box mx={5}>
+      <Box sx={{ fontWeight: 'bold', textAlign: 'center', m: 3 }}>Aim</Box>
+      <Typography variant="body1" align="justify" marginBottom={1}>
+        <b>Aim:</b> To Simulate virtually and Interpret Binary Phase Shift Keying waveforms. 
+      </Typography>
+    </Box>
   )
 }
 
