422e846375fb3ed9492333d399f7231a8146a6a1
omkark4004 <o.kubal@somaiya.edu>
2023-09-07 01:21:50 +0530

    Updates

---
+++ precache-manifest.ac891831e7d24ae327288ec13f3b43bf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c22868d66d15ef8eb7f",
+    "url": "/DCOM-1-PSK/static/js/main.3c22868d.chunk.js"
+  },
+  {
+    "revision": "8619273fcb159707cb36",
+    "url": "/DCOM-1-PSK/static/js/1.8619273f.chunk.js"
+  },
+  {
+    "revision": "d0dbf4f2bb76664ca803",
+    "url": "/DCOM-1-PSK/static/js/runtime~main.d0dbf4f2.js"
+  },
+  {
+    "revision": "8741e85a9a6227bef12389aeafc122f4",
+    "url": "/DCOM-1-PSK/static/media/engg-logo.8741e85a.png"
+  },
+  {
+    "revision": "281bbf63a1292384e8e5fd1c21ea3bcc",
+    "url": "/DCOM-1-PSK/static/media/bpsk.281bbf63.png"
+  },
+  {
+    "revision": "0d6ee88c2504a3b047eff402fe2fb886",
+    "url": "/DCOM-1-PSK/static/media/blockdiagbpsk.0d6ee88c.png"
+  },
+  {
+    "revision": "734365eb518d404549a5283ecbc7a7e2",
+    "url": "/DCOM-1-PSK/static/media/spectrum.734365eb.png"
+  },
+  {
+    "revision": "61b9f855b9354d6ce7f0a94263bb8967",
+    "url": "/DCOM-1-PSK/static/media/simulation2.61b9f855.jpeg"
+  },
+  {
+    "revision": "5fcf3fff169e6b6975fd215a4a7fea6f",
+    "url": "/DCOM-1-PSK/static/media/aim.5fcf3fff.png"
+  },
+  {
+    "revision": "fb8dcfd21ba34f21f1bb4825e77770fb",
+    "url": "/DCOM-1-PSK/static/media/bpsk-deM.fb8dcfd2.png"
+  },
+  {
+    "revision": "6e5ea59001a15dda86fde231b01ec21d",
+    "url": "/DCOM-1-PSK/static/media/feedback.6e5ea590.png"
+  },
+  {
+    "revision": "edd1caf753acdc51531914a087068585",
+    "url": "/DCOM-1-PSK/static/media/modulated-bpsk.edd1caf7.png"
+  },
+  {
+    "revision": "60356def1b7b3e2927de1d2e64d2bd4e",
+    "url": "/DCOM-1-PSK/static/media/post-test.60356def.png"
+  },
+  {
+    "revision": "209eac3265e17e6a52949d3f9f923e99",
+    "url": "/DCOM-1-PSK/static/media/pre-test.209eac32.png"
+  },
+  {
+    "revision": "39d20a64c422032136941794086691be",
+    "url": "/DCOM-1-PSK/static/media/procedure.39d20a64.png"
+  },
+  {
+    "revision": "b95fc1e2e40c4475a4c06b10ccc71f76",
+    "url": "/DCOM-1-PSK/static/media/references.b95fc1e2.png"
+  },
+  {
+    "revision": "ad267ccd162e5fe7b192940400effe3d",
+    "url": "/DCOM-1-PSK/static/media/simulation.ad267ccd.png"
+  },
+  {
+    "revision": "bbd68312e5c9b15662abfc9488ecbf1e",
+    "url": "/DCOM-1-PSK/static/media/theory.bbd68312.png"
+  },
+  {
+    "revision": "c099ca59a7e56199eba2e2b8c7cf4054",
+    "url": "/DCOM-1-PSK/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/DCOM-1-PSK/precache-manifest.ac891831e7d24ae327288ec13f3b43bf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/DCOM-1-PSK/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
